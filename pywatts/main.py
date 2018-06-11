c2da05a9b7c3d9688f25fab30954d16c8ea9e079
reedts <j.reedts@gmail.com>
2018-06-11 16:32:58 +0200

    Added function to convert input json to dataframe

--- pywatts/main.py
+++ pywatts/main.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
 import matplotlib.pyplot as pp
 import pywatts.neural
+import pandas
 
 from sklearn.model_selection import train_test_split
+
 
 df = pywatts.db.rows_to_df(list(range(1, 50)))
 X = df[[col for col in df.columns if col != 'dc']]
@@ -12,10 +14,18 @@ X_train, X_tmp, y_train, y_tmp = train_test_split(X, y, test_size=0.2, random_st
 
 X_test, X_val, y_test, y_val = train_test_split(X_tmp, y_tmp, test_size=0.5, random_state=23)
 
-X_train.shape, X_test.shape, X_val.shape
-
 feature_cols = [tf.feature_column.numeric_column(col) for col in X.columns]
 n = pywatts.neural.Net(feature_cols=feature_cols)
+
+
+def input_data(json_str):
+    tmp_df = pandas.read_json(json_str)
+
+    return pandas.DataFrame.from_dict(
+        {'dc': [x for l in tmp_df['dc'] for x in l],
+         'temp': [x for l in tmp_df['temp'] for x in l],
+         'wind': [x for l in tmp_df['wind'] for x in l]}
+    )
 
 
 def train(steps=100):
