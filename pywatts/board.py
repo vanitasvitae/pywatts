fd623c32de511cdde8ef27fc8e071631b8d4a158
Paul Schaub <vanitasvitae@fsfe.org>
2018-08-13 12:38:22 +0200

    Add tensorboard export script

---
+++ pywatts/board.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import subprocess
+
+writer = tf.summary.FileWriter("tensorboard")
+checkpoint = tf.train.get_checkpoint_state('tf_pywatts_model')
+with tf.Session() as sess:
+    saver = tf.train.import_meta_graph(checkpoint.model_checkpoint_path + '.meta')
+    saver.restore(sess, checkpoint.model_checkpoint_path)
+writer.add_graph(sess.graph)
+
+subprocess.check_output(['tensorboard', '--logdir', 'tensorboard'])
